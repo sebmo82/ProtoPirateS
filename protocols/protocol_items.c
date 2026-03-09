5933d9524f9cf6acf41be1b0a3ffc3e33226042a
sebmo82 <seb-mo@hotmail.fr>
2026-03-09 20:27:10 +0100

    Update protocol_items.c

--- protocols/protocol_items.c
+++ protocols/protocol_items.c
@@ -1,9 +1,9 @@
 #include "protocol_items.h"
-#include "toyota_2008.h" // Importation du nouveau header
+#include "protocoles/toyota_2008.h" // Chemin complet vers le sous-dossier
 #include <string.h>
 
 const SubGhzProtocol* protopirate_protocol_registry_items[] = {
-    &subghz_protocol_toyota_2008, // Ajout en haut de liste
+    &subghz_protocol_toyota_2008, // Enregistrement du nouveau décodeur
     &subghz_protocol_scher_khan,
     &kia_protocol_v0,
     &kia_protocol_v1,
@@ -20,17 +20,4 @@ const SubGhzProtocol* protopirate_protocol_registry_items[] = {
     &psa_protocol,
 };
 
-static const ProtoPirateProtocolTiming protocol_timings[] = {
-    // Ajout des timings pour l'analyseur Toyota
-    {
-        .name = "Toyota 2008",
-        .te_short = 440,
-        .te_long = 1200,
-        .te_delta = 150,
-        .min_count_bit = 64,
-    },
-    // ... garde tes autres timings ici sans rien supprimer
-    {
-        .name = "Kia V0",
-        .te_short = 250,
-        // ...
+// ... garde le reste du fichier (protocol_timings, etc.) identique
