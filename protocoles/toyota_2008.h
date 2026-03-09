7a57d655c622ce764f2e974d58d6b6ff5b20d23b
sebmo82 <seb-mo@hotmail.fr>
2026-03-09 20:30:51 +0100

    Update toyota_2008.h

--- protocoles/toyota_2008.h
+++ protocoles/toyota_2008.h
@@ -1,15 +1,15 @@
-#pragma once
+#ifndef TOYOTA_2008_H
+#define TOYOTA_2008_H
 
 #include <lib/subghz/protocols/base.h>
 #include <lib/subghz/blocks/generic.h>
+#include <lib/subghz/types.h>
 
-/**
- * Forward declaration pour le compilateur
- * Evite l'erreur 'unknown type name SubGhzReceiver'
- */
 struct SubGhzReceiver;
 typedef struct SubGhzReceiver SubGhzReceiver;
 
 extern const SubGhzProtocol subghz_protocol_toyota_2008;
 
 bool subghz_protocol_toyota_2008_decode(SubGhzBlockGeneric* instance, SubGhzReceiver* receiver);
+
+#endif // TOYOTA_2008_H
