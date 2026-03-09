dd9ce952123a9b1b9392d971e458b228beb38b4f
sebmo82 <seb-mo@hotmail.fr>
2026-03-09 20:22:22 +0100

    Update toyota_2008.h

--- protocoles/toyota_2008.h
+++ protocoles/toyota_2008.h
@@ -3,8 +3,9 @@
 #include <lib/subghz/protocols/base.h>
 #include <lib/subghz/blocks/generic.h>
 
-// Déclaration globale du protocole pour protocol_items.c
+// Déclaration anticipée pour le compilateur
+typedef struct SubGhzReceiver SubGhzReceiver;
+
 extern const SubGhzProtocol subghz_protocol_toyota_2008;
 
-// Prototypes requis par le SDK
 bool subghz_protocol_toyota_2008_decode(SubGhzBlockGeneric* instance, SubGhzReceiver* receiver);
